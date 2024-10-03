6b2a3835261ea0e277bc6b95009d9dbee0a410f5
delterr <Hasan.T.Ahmad@ons.gov.uk>
2024-10-03 15:37:35 +0100

    dashboard init

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ load-design-system-templates:
 	./scripts/load_release.sh onsdigital/design-system $(DESIGN_SYSTEM_VERSION)
 
 run: load-design-system-templates
-	flask --app application run
+	poetry run flask --app application run --debug
 
 format-python:
 	poetry run isort .
--- application.py
+++ application.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from jinja2 import ChainableUndefined
 
 app = Flask(__name__)
+app.secret_key = "lsdbfnhjldfbvjlhdsblhjsd"
 app.jinja_env.undefined = ChainableUndefined
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", param="Hello world")
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if password == "":
+        flash("Password cannot be empty")
+        return redirect(url_for("home"))
+
+    session["email"] = email
+    session["password"] = password
+
+    flash("You have successfully logged in")
+    return redirect(url_for("dashboard"))
+
+@app.route("/dashboard")
+def dashboard():
+
+    return render_template("dashboard.html", email=session["email"], password=session["password"])
+
 
 
 if __name__ == "__main__":

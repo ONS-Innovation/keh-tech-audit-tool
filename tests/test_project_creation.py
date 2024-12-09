e9c5885fd13c97984f5faad4b2b85225b4ab9e0c
delterr <Hasan.T.Ahmad@ons.gov.uk>
2024-12-09 13:45:02 +0000

    Added wait.until() for button clicks

--- tests/test_project_creation.py
+++ tests/test_project_creation.py
@@ -17,7 +17,7 @@ class TestProjectCreation(unittest.TestCase):
         self.driver.get("http://localhost:8000")
         self.email = os.getenv("TEST_EMAIL")
         self.password = os.getenv("TEST_PASSWORD")
-        # self.driver.add_cookie({"name": "session", "value": id_token})
+        self.wait = WebDriverWait(self.driver, 5)
         self.driver.refresh()
 
     def test_project_details(self):
@@ -26,18 +26,16 @@ class TestProjectCreation(unittest.TestCase):
 
         self.click_link(driver, "Sign in with Cognito")
 
-        wait = WebDriverWait(self.driver, 5)
-
-        form = wait.until(
+        form = self.wait.until(
             EC.visibility_of_element_located((By.XPATH, "//div[contains(@class,'visible-md')]//form[@name='cognitoSignInForm']"))
         )
+
         email = form.find_element(By.NAME, "username")
         password = form.find_element(By.NAME, "password")
         email.send_keys(self.email)
         password.send_keys(self.password)
         button = form.find_element(By.NAME, "signInSubmitButton")
-        button.click()
-        time.sleep(3)
+        self.wait.until(EC.element_to_be_clickable(button)).click()
 
         driver.get("http://localhost:8000/survey")
 
@@ -51,7 +49,8 @@ class TestProjectCreation(unittest.TestCase):
 
         link = driver.find_elements(By.CLASS_NAME, "ons-summary__button")[1]
 
-        link.click()
+        self.wait.until(EC.element_to_be_clickable(link)).click()
+
 
         driver.implicitly_wait(10)
 
@@ -65,7 +64,7 @@ class TestProjectCreation(unittest.TestCase):
 
         link = driver.find_elements(By.CLASS_NAME, "ons-summary__button")[2]
 
-        link.click()
+        self.wait.until(EC.element_to_be_clickable(link)).click()
 
         self.click_link(driver, "Continue")
 
@@ -83,7 +82,7 @@ class TestProjectCreation(unittest.TestCase):
 
         button = driver.find_element(By.ID, 'submit-button')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        button.click()
+        self.wait.until(EC.element_to_be_clickable(button)).click()
         time.sleep(10)
 
     def tearDown(self):
@@ -93,14 +92,14 @@ class TestProjectCreation(unittest.TestCase):
         """Click link via text"""
         link = driver.find_element(By.LINK_TEXT, link_text)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        link.click()
+        self.wait.until(EC.element_to_be_clickable(link)).click()
         driver.implicitly_wait(10)
 
     def click_radio(self, driver, options):
         """Randomly choose option from radio"""
         choice = random.choice(options)
         radio = driver.find_element(By.ID, choice)
-        radio.click()
+        self.wait.until(EC.element_to_be_clickable(radio)).click()
         return choice
 
     def assert_validation_page(self):
@@ -131,35 +130,35 @@ class TestProjectCreation(unittest.TestCase):
         """Complete contact details i.e technical contact and delivery manager"""
         link = driver.find_elements(By.CLASS_NAME, "ons-summary__button")[0]
         print(link.text)
-        link.click()
-        
+        self.wait.until(EC.element_to_be_clickable(link)).click()
+
         driver.implicitly_wait(10)
         link = driver.find_element(By.LINK_TEXT, "Continue")
-        link.click()
-        
+        self.wait.until(EC.element_to_be_clickable(link)).click()
+
         driver.implicitly_wait(10)
         email = driver.find_element(By.ID, "contact-email")
-        email.click()
+        self.wait.until(EC.element_to_be_clickable(email)).click()
         email.send_keys("test@ons.gov.uk")
 
         choice = self.click_radio(driver,  ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
 
         if choice == "other":
             other_input = driver.find_element(By.ID, "other-input")
-            other_input.click()
+            self.wait.until(EC.element_to_be_clickable(other_input)).click()
             other_input.send_keys("Example Role")
         
         self.click_link(driver, "Save and continue")
     
         email = driver.find_element(By.ID, "contact-email")
-        email.click()
+        self.wait.until(EC.element_to_be_clickable(email)).click()
         email.send_keys("testmanager@ons.gov.uk")
 
         choice = self.click_radio(driver, ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
 
         if choice == "other":
             other_input = driver.find_element(By.ID, "other-input")
-            other_input.click()
+            self.wait.until(EC.element_to_be_clickable(other_input)).click()
             other_input.send_keys("Example Role")
         
         self.click_link(driver, "Save and continue")
@@ -171,13 +170,13 @@ class TestProjectCreation(unittest.TestCase):
         documentation_link = driver.find_element(By.ID, "documentation-link")
         project_description = driver.find_element(By.ID, "project-description")
 
-        project_name.click()
+        self.wait.until(EC.element_to_be_clickable(project_name)).click()
         project_name.send_keys("Selenium bot test")
-        project_short_name.click()
+        self.wait.until(EC.element_to_be_clickable(project_short_name)).click()
         project_short_name.send_keys("selbottest")
-        documentation_link.click()
+        self.wait.until(EC.element_to_be_clickable(documentation_link)).click()
         documentation_link.send_keys("https://example.com")
-        project_description.click()
+        self.wait.until(EC.element_to_be_clickable(project_description)).click()
         project_description.send_keys("https://example.com")
         self.click_link(driver, "Save and continue")
 
@@ -187,11 +186,11 @@ class TestProjectCreation(unittest.TestCase):
 
         if choice == "partnership":
             company_name = driver.find_element(By.ID, "other-input-2")
-            company_name.click()
+            self.wait.until(EC.element_to_be_clickable(company_name)).click()
             company_name.send_keys("Example Company")
         elif choice == "outsourced":
             company_name = driver.find_element(By.ID, "other-input-1")
-            company_name.click()
+            self.wait.until(EC.element_to_be_clickable(company_name)).click()
             company_name.send_keys("Example Company")
 
         self.click_link(driver, "Save and continue")
@@ -208,22 +207,22 @@ class TestProjectCreation(unittest.TestCase):
 
         if choice == "other":
             source_control = driver.find_element(By.ID, "other-input")
-            source_control.click()
+            self.wait.until(EC.element_to_be_clickable(source_control)).click()
             source_control.send_keys("Bitbucket")
         
         self.click_link(driver, "Save and continue")
 
         source_control_link = driver.find_element(By.ID, "source_control_link-input")
-        source_control_link.click()
+        self.wait.until(EC.element_to_be_clickable(source_control_link)).click()
         source_control_link.send_keys("https://example.com")
         
         source_control_description = driver.find_element(By.ID, "source_control_desc-input")
-        source_control_description.click()
+        self.wait.until(EC.element_to_be_clickable(source_control_description)).click()
         source_control_description.send_keys("Test Description")
 
         # add_btn = driver.find_elements(By.CLASS_NAME, "ons-btn")[0]
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         self.click_link(driver, "Save and continue")
 
     def complete_hosting(self, driver):
@@ -235,22 +234,21 @@ class TestProjectCreation(unittest.TestCase):
             hosting_provider = driver.find_element(By.ID, "hosting-input")
             time.sleep(5)
             driver.implicitly_wait(10)
-            hosting_provider.click()
+            self.wait.until(EC.element_to_be_clickable(hosting_provider)).click()
             hosting_provider.send_keys("Example Hosting Provider")
 
             add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-            add_btn.click()
             self.click_link(driver, "Save and continue")
         driver.implicitly_wait(10)
 
     def complete_database(self, driver):
         """Complete details on datbases used"""
         database = driver.find_element(By.ID, "database-input")
-        database.click()
+        self.wait.until(EC.element_to_be_clickable(database)).click()
         database.send_keys("Example Database Provider")
 
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         driver.implicitly_wait(10)
 
         self.click_link(driver, "Save and continue")
@@ -261,65 +259,55 @@ class TestProjectCreation(unittest.TestCase):
         """Complete details on languages used"""
         language = driver.find_element(By.ID, "languages-input")
 
-        language.click()
+        self.wait.until(EC.element_to_be_clickable(language)).click()
 
         language.send_keys("Python")
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
-
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         driver.implicitly_wait(10)
 
         main_language = driver.find_element(By.XPATH, '//input[@value="main"]')
-        main_language.click()
-
+        self.wait.until(EC.element_to_be_clickable(main_language)).click()
         language = driver.find_element(By.ID, "languages-input")
 
-        language.click()
-
+        self.wait.until(EC.element_to_be_clickable(language)).click()
         language.send_keys("JavaScript")
 
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
-
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         self.click_link(driver, "Save and continue")
 
     def complete_frameworks(self, driver):
         """Complete details on frameworks used"""
         framework = driver.find_element(By.ID, "frameworks-input")
 
-        framework.click()
-
+        self.wait.until(EC.element_to_be_clickable(framework)).click()
         framework.send_keys("Django")
 
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
-
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         self.click_link(driver, "Save and continue")
         
     def complete_integrations(self, driver):
         """Completes the integrations section of the project creation form."""
         integrations = driver.find_element(By.ID, "integrations-input")
         
-        integrations.click()
-
+        self.wait.until(EC.element_to_be_clickable(integrations)).click()
         integrations.send_keys("Github Actions")
 
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
-
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         self.click_link(driver, "Save and continue")
 
     def complete_infrastructure(self, driver):
         """Completes the infrastructure section of the project creation process."""
         infrastructure = driver.find_element(By.ID, "infrastructure-input")
         
-        infrastructure.click()
-
+        self.wait.until(EC.element_to_be_clickable(infrastructure)).click()
         infrastructure.send_keys("AWS")
 
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
-        add_btn.click()
-
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
         self.click_link(driver, "Save and continue")
 
         self.click_link(driver, "Finish section")

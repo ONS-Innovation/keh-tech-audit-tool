9d2e4960f6807125207e96fa479c7e795e86c062
delterr <Hasan.T.Ahmad@ons.gov.uk>
2024-12-09 07:18:39 +0000

    Moved functions into methods of test class

--- tests/test_project_creation.py
+++ tests/test_project_creation.py
@@ -13,7 +13,6 @@ class TestProjectCreation(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        id_token = os.getenv("id_token")
         self.driver.get("http://localhost:8000")
         self.email = os.getenv("TEST_EMAIL")
         self.password = os.getenv("TEST_PASSWORD")
@@ -23,7 +22,7 @@ class TestProjectCreation(unittest.TestCase):
     def test_project_details(self):
         driver = self.driver
 
-        click_link(driver, "Sign in with Cognito")
+        self.click_link(driver, "Sign in with Cognito")
 
         wait = WebDriverWait(self.driver, 3)
 
@@ -40,11 +39,11 @@ class TestProjectCreation(unittest.TestCase):
 
         driver.get("http://localhost:8000/survey")
 
-        complete_contact_details(driver)
+        self.complete_contact_details(driver)
 
-        complete_project_details(driver)
+        self.complete_project_details(driver)
 
-        complete_tools_details(driver)
+        self.complete_tools_details(driver)
 
         driver.implicitly_wait(10)
 
@@ -54,29 +53,29 @@ class TestProjectCreation(unittest.TestCase):
 
         driver.implicitly_wait(10)
 
-        click_link(driver, "Continue")
+        self.click_link(driver, "Continue")
 
-        complete_source_control(driver)
+        self.complete_source_control(driver)
 
-        complete_hosting(driver)
+        self.complete_hosting(driver)
 
-        complete_database(driver)
+        self.complete_database(driver)
 
         link = driver.find_elements(By.CLASS_NAME, "ons-summary__button")[2]
 
         link.click()
 
-        click_link(driver, "Continue")
+        self.click_link(driver, "Continue")
 
-        complete_languages(driver)
+        self.complete_languages(driver)
         
-        complete_frameworks(driver)
+        self.complete_frameworks(driver)
 
-        complete_integrations(driver)
+        self.complete_integrations(driver)
 
-        complete_infrastructure(driver)
+        self.complete_infrastructure(driver)
 
-        click_link(driver, "Continue to Submission")
+        self.click_link(driver, "Continue to Submission")
 
         button = driver.find_element(By.ID, 'submit-button')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -86,20 +85,20 @@ class TestProjectCreation(unittest.TestCase):
     
     def tearDown(self):
         self.driver.close()
+    
+    def click_link(self, driver, link_text):
+        link = driver.find_element(By.LINK_TEXT, link_text)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        link.click()
+        driver.implicitly_wait(10)
 
-def click_link(driver, link_text):
-    link = driver.find_element(By.LINK_TEXT, link_text)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    link.click()
-    driver.implicitly_wait(10)
+    def click_radio(self, driver, options):
+        choice = random.choice(options)
+        radio = driver.find_element(By.ID, choice)
+        radio.click()
+        return choice
 
-def click_radio(driver, options):
-    choice = random.choice(options)
-    radio = driver.find_element(By.ID, choice)
-    radio.click()
-    return choice
-
-def complete_contact_details(driver):
+    def complete_contact_details(self, driver):
         link = driver.find_elements(By.CLASS_NAME, "ons-summary__button")[0]
         print(link.text)
         link.click()
@@ -113,29 +112,29 @@ def complete_contact_details(driver):
         email.click()
         email.send_keys("test@ons.gov.uk")
 
-        choice = click_radio(driver,  ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
+        choice = self.click_radio(driver,  ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
 
         if choice == "other":
             other_input = driver.find_element(By.ID, "other-input")
             other_input.click()
             other_input.send_keys("Example Role")
         
-        click_link(driver, "Save and continue")
-       
+        self.click_link(driver, "Save and continue")
+    
         email = driver.find_element(By.ID, "contact-email")
         email.click()
         email.send_keys("testmanager@ons.gov.uk")
 
-        choice = click_radio(driver, ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
+        choice = self.click_radio(driver, ['Grade 6', "Grade 7", "SEO", "HEO", "other"])
 
         if choice == "other":
             other_input = driver.find_element(By.ID, "other-input")
             other_input.click()
             other_input.send_keys("Example Role")
         
-        click_link(driver, "Save and continue")
-    
-def complete_project_details(driver):
+        self.click_link(driver, "Save and continue")
+        
+    def complete_project_details(self, driver):
         project_name = driver.find_element(By.ID, "project-name")
         project_short_name = driver.find_element(By.ID, "project-short-name")
         documentation_link = driver.find_element(By.ID, "documentation-link")
@@ -149,10 +148,10 @@ def complete_project_details(driver):
         documentation_link.send_keys("https://example.com")
         project_description.click()
         project_description.send_keys("test description")
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-def complete_tools_details(driver):
-        choice = click_radio(driver, ["in-house", "outsourced", "partnership"])
+    def complete_tools_details(self, driver):
+        choice = self.click_radio(driver, ["in-house", "outsourced", "partnership"])
 
         if choice == "partnership":
             company_name = driver.find_element(By.ID, "other-input-2")
@@ -163,23 +162,23 @@ def complete_tools_details(driver):
             company_name.click()
             company_name.send_keys("Example Company")
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-        click_radio(driver, ["Development", "Active Support", "Unsupported"])
+        self.click_radio(driver, ["Development", "Active Support", "Unsupported"])
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-        click_link(driver, "Finish section")
+        self.click_link(driver, "Finish section")
 
-def complete_source_control(driver):
-        choice = click_radio(driver, ["github", "gitlab", "other"])
+    def complete_source_control(self, driver):
+        choice = self.click_radio(driver, ["github", "gitlab", "other"])
 
         if choice == "other":
             source_control = driver.find_element(By.ID, "other-input")
             source_control.click()
             source_control.send_keys("Bitbucket")
         
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
         source_control_link = driver.find_element(By.ID, "source_control_link-input")
         source_control_link.click()
@@ -192,11 +191,11 @@ def complete_source_control(driver):
         # add_btn = driver.find_elements(By.CLASS_NAME, "ons-btn")[0]
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
         add_btn.click()
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-def complete_hosting(driver):
-        click_radio(driver, ["On-premises", "Cloud", "Hybrid"])
-        click_link(driver, "Save and continue")
+    def complete_hosting(self, driver):
+        self.click_radio(driver, ["On-premises", "Cloud", "Hybrid"])
+        self.click_link(driver, "Save and continue")
 
         hosting_provider = driver.find_element(By.ID, "hosting-input")
         driver.implicitly_wait(10)
@@ -207,9 +206,9 @@ def complete_hosting(driver):
         add_btn.click()
         driver.implicitly_wait(10)
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-def complete_database(driver):
+    def complete_database(self, driver):
         database = driver.find_element(By.ID, "database-input")
         database.click()
         database.send_keys("Example Database Provider")
@@ -218,11 +217,11 @@ def complete_database(driver):
         add_btn.click()
         driver.implicitly_wait(10)
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-        click_link(driver, "Finish section")
-    
-def complete_languages(driver):
+        self.click_link(driver, "Finish section")
+        
+    def complete_languages(self, driver):
         language = driver.find_element(By.ID, "languages-input")
 
         language.click()
@@ -245,9 +244,9 @@ def complete_languages(driver):
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
         add_btn.click()
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-def complete_frameworks(driver):
+    def complete_frameworks(self, driver):
         framework = driver.find_element(By.ID, "frameworks-input")
 
         framework.click()
@@ -257,9 +256,9 @@ def complete_frameworks(driver):
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
         add_btn.click()
 
-        click_link(driver, "Save and continue")
-    
-def complete_integrations(driver):
+        self.click_link(driver, "Save and continue")
+        
+    def complete_integrations(self, driver):
         integrations = driver.find_element(By.ID, "integrations-input")
         
         integrations.click()
@@ -269,9 +268,9 @@ def complete_integrations(driver):
         add_btn = driver.find_element(By.XPATH, '//button[@class="ons-btn ons-search__btn ons-btn--small"]')
         add_btn.click()
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
-def complete_infrastructure(driver):
+    def complete_infrastructure(self, driver):
         infrastructure = driver.find_element(By.ID, "infrastructure-input")
         
         infrastructure.click()
@@ -282,10 +281,10 @@ def complete_infrastructure(driver):
         add_btn.click()
 
 
-        click_link(driver, "Save and continue")
+        self.click_link(driver, "Save and continue")
 
 
-        click_link(driver, "Finish section")
+        self.click_link(driver, "Finish section")
 
 
 if __name__ == "__main__":

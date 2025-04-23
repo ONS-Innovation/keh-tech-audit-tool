e40db10ca8d7b6bb0855c53f714e15c1ed37d42a
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-04-23 16:12:31 +0100

    test bash script

---
+++ concourse/scripts/terraform_infra.bash
@@ -0,0 +1,20 @@
+export AWS_ACCESS_KEY_ID=((aws_access_key_id))
+export AWS_SECRET_ACCESS_KEY=((aws_secret_access_key))
+git config --global url."https://x-access-token:((github_access_token))@github.com/".insteadOf "https://github.com/"
+
+cd resource-repo/terraform/service
+terraform init -backend-config=env/dev/backend-dev.tfbackend -reconfigure
+terraform apply \
+-var "aws_account_id=((aws_account_id))" \
+-var "aws_access_key_id=((aws_access_key_id))" \
+-var "aws_secret_access_key=((aws_secret_access_key))" \
+-var "api_secret_name=((api_secret_name))" \
+-var "domain=((domain))" \
+-var "container_ver=((container_ver))" \
+-var "api_bucket_name=((api_bucket_name))" \
+-var "ui_secret_name=((ui_secret_name))" \
+-var "aws_account_name=((aws_account_name))" \
+-var "localhost=((localhost))" \
+-var "container_image=((container_image))" \
+-var "force_deployment=((force_deployment))" \
+-auto-approve

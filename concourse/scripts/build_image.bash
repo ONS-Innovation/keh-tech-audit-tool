030fc0acb6f0708eaf0e4fa3bd8b1f971ac00ae2
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-08 15:59:28 +0100

    remove redundant lines

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -3,9 +3,8 @@ cd ../..
 
 aws ecr get-login-password --region region | podman login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.region.amazonaws.com
 
-podman pull ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
-
-podman run -d ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
+podman build -t tech_audit_tool:latest .
 
 podman tag tech_audit_tool:latest ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
+
 podman push ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest

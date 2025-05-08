21619eac84e372163186e516468576ed490a8629
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-08 12:24:47 +0100

    replace args

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -1,9 +1,6 @@
 
 cd ../..
 
-./concourse/scripts/assume_role.bash
-
-
 docker build -t tech-audit-tool .
 
 docker tag tech-audit-tool:latest ${account_id}.dkr.ecr.eu-west-2.amazonaws.com/tech-audit-tool:latest

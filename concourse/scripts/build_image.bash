e1e13e47d35ce84e7e40895a52a95be8e91d41a8
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-08 17:03:07 +0100

    remove stray quote mark

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -12,4 +12,4 @@ podman build -t tech_audit_tool:latest resource-repo
 
 podman tag tech_audit_tool:latest ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
 
-podman push ${aws_account_id}".dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
+podman push ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest

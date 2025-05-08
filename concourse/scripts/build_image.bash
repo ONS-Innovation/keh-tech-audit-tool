e4380149f4d4a60acc1fe61be84b2ea7c0a7e871
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-08 16:51:45 +0100

    let podman use vfs driver

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -7,6 +7,10 @@ echo ${aws_account_id}
 
 aws ecr get-login-password --region eu-west-2 | podman login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
+export STORAGE_DRIVER=vfs
+export PODMAN_SYSTEMD_UNIT=concourse-task
+podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
+
 podman build -t tech_audit_tool:latest .
 
 podman tag tech_audit_tool:latest ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest

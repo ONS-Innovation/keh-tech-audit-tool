fcdd6e50c47921b93f640d38b5758331250d235e
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-08 16:19:00 +0100

    test to see if account id is being output

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -3,10 +3,12 @@ set -x
 
 cd ../..
 
-aws ecr get-login-password --region eu-west-2 | podman login --username AWS --password-stdin "${aws_account_id}".dkr.ecr.eu-west-2.amazonaws.com
+echo ${aws_account_id}
+
+aws ecr get-login-password --region eu-west-2 | podman login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
 podman build -t tech_audit_tool:latest .
 
-podman tag tech_audit_tool:latest "${aws_account_id}".dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
+podman tag tech_audit_tool:latest ${aws_account_id}.dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
 
-podman push "${aws_account_id}".dkr.ecr.region.amazonaws.com/tech_audit_tool:latest
+podman push ${aws_account_id}".dkr.ecr.region.amazonaws.com/tech_audit_tool:latest

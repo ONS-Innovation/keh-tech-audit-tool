04c489bc5cc4f4393e7c4329faddd43cd4ee1a50
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-09 14:59:43 +0100

    merge assume role and build image together?

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -1,6 +1,19 @@
 set -e
 set -x
 
+apk add --no-cache aws-cli podman
+
+aws sts assume-role --output text \
+    --role-arn "${aws_role_arn}" \
+    --role-session-name concourse-pipeline-run \
+    --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+    | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
+
+
+export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
+export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
+export AWS_SESSION_TOKEN="$(cat SessionToken)"
+
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task
 

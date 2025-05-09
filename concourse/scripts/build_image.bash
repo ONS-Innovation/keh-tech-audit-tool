26eea3e1abf9c684863996c61d6cc9b09e14b2c1
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-09 10:28:52 +0100

    use only build_image.bash

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -1,7 +1,26 @@
 set -e
 set -x
 
-echo ${aws_account_id}
+apk add --no-cache aws-cli podman
+
+# Extract credentials from the given user
+
+echo "${aws_account_id}"
+
+aws sts assume-role --output text \
+    --role-arn "${aws_role_arn}" \
+    --role-session-name concourse-pipeline-run \
+    --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+    | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
+
+
+export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
+export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
+export AWS_SESSION_TOKEN="$(cat SessionToken)"
+
+echo "${AWS_ACCESS_KEY_ID}"
+echo "${AWS_SECRET_ACCESS_KEY}"
+echo "${AWS_SESSION_TOKEN}"
 
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task

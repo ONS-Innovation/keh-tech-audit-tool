96c68fe5f062c0b330a0bf50654aed7bf9aed6b9
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-09 10:12:38 +0100

    replace _ with -

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -8,7 +8,7 @@ export PODMAN_SYSTEMD_UNIT=concourse-task
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
-podman build -t tech_audit_tool:latest resource-repo
+podman build -t tech-audit-tool:latest resource-repo
 
 podman tag tech_audit_tool:latest ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/tech-audit-tool:latest
 

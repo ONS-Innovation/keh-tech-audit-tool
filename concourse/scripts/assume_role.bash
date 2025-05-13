54c2c942827836013770efa30844211f1e9693f2
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-13 11:18:14 +0100

    separate out build_image and assume_role, make set-pipeline script

--- concourse/scripts/assume_role.bash
+++ concourse/scripts/assume_role.bash
@@ -4,24 +4,12 @@ set -x
 
 apk add --no-cache aws-cli podman
 
-# Extract credentials from the given user
-
-echo "${aws_account_id}"
-
 aws sts assume-role --output text \
     --role-arn "${aws_role_arn}" \
     --role-session-name concourse-pipeline-run \
     --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
     | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
 
-
 export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
 export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
 export AWS_SESSION_TOKEN="$(cat SessionToken)"
-
-aws sts get-caller-identity --output=json
-
-
-echo "${AWS_ACCESS_KEY_ID}"
-echo "${AWS_SECRET_ACCESS_KEY}"
-echo "${AWS_SESSION_TOKEN}"
--- concourse/scripts/set_pipeline.bash
+++ concourse/scripts/set_pipeline.bash
@@ -1,7 +1,12 @@
 tag=$(git rev-parse HEAD)
 
-if [ ${#1} -gt 0 ]; then
+if [[ ${#1} -gt 0 ]]; then
     branch=${1}
+    git rev-parse --verify ${branch}
+    if [[ $? -ne 0 ]];  then
+        echo "Branch \"${branch}\" does not exist"
+        exit 1
+    fi
 else
     branch=$(git rev-parse --abbrev-ref HEAD)
 fi

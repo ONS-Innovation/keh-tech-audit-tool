3696e68c189bbe38c2da450909f2c59af93abdf0
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-23 14:51:51 +0100

    use tag file instead

--- concourse/scripts/build_image.bash
+++ concourse/scripts/build_image.bash
@@ -3,7 +3,7 @@ set -euo pipefail
 export STORAGE_DRIVER=vfs
 export PODMAN_SYSTEMD_UNIT=concourse-task
 
-echo ${tag}
+tag=$(cat tag.txt)
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 

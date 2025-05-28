a980109838a69af3638f867861e44c64aa388307
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-05-28 15:36:20 +0100

    update set_pipeline to use hyphen

--- concourse/scripts/set_pipeline.bash
+++ concourse/scripts/set_pipeline.bash
@@ -12,7 +12,7 @@ else
     branch=$(git rev-parse --abbrev-ref HEAD)
 fi
 
-if [[ ${branch} == "main" || ${branch} == "master" || "concourse_prod" ]]; then
+if [[ ${branch} == "main" || ${branch} == "master" || "concourse-prod" ]]; then
     env="prod"
 else
     env="dev"

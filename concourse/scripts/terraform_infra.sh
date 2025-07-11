83bdada1712af9db312454b8d2c9afdfd5cb8ac3
Hasan <31344273+delterr@users.noreply.github.com>
2025-07-11 11:47:51 +0100

    Concourse (#39)
    Update Concourse Pipeline

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -1,23 +1,21 @@
 set -euo pipefail
 
-apk add --no-cache jq
+aws_account_id=$(echo "$secrets" | jq -r .aws_account_id)
+aws_access_key_id=$(echo "$secrets" | jq -r .aws_access_key_id)
 
-aws_account_id=$(echo "$tat_secrets_ui" | jq -r .aws_account_id)
-aws_access_key_id=$(echo "$tat_secrets_ui" | jq -r .aws_access_key_id)
+aws_secret_access_key=$(echo "$secrets" | jq -r .aws_secret_access_key)
+api_secret_name=$(echo "$secrets" | jq -r .api_secret_name)
 
-aws_secret_access_key=$(echo "$tat_secrets_ui" | jq -r .aws_secret_access_key)
-api_secret_name=$(echo "$tat_secrets_ui" | jq -r .api_secret_name)
+domain=$(echo "$secrets" | jq -r .domain)
 
-domain=$(echo "$tat_secrets_ui" | jq -r .domain)
+api_bucket_name=$(echo "$secrets" | jq -r .api_bucket_name)
+ui_secret_name=$(echo "$secrets" | jq -r .ui_secret_name)
 
-api_bucket_name=$(echo "$tat_secrets_ui" | jq -r .api_bucket_name)
-ui_secret_name=$(echo "$tat_secrets_ui" | jq -r .ui_secret_name)
+aws_account_name=$(echo "$secrets" | jq -r .aws_account_name)
+localhost=$(echo "$secrets" | jq -r .localhost)
 
-aws_account_name=$(echo "$tat_secrets_ui" | jq -r .aws_account_name)
-localhost=$(echo "$tat_secrets_ui" | jq -r .localhost)
-
-container_image=$(echo "$tat_secrets_ui" | jq -r .container_image)
-force_deployment=$(echo "$tat_secrets_ui" | jq -r .force_deployment)
+container_image=$(echo "$secrets" | jq -r .container_image)
+force_deployment=$(echo "$secrets" | jq -r .force_deployment)
 
 export AWS_ACCESS_KEY_ID=$aws_access_key_id
 export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key

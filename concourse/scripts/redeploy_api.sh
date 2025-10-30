5ed8eaa4350710b03b0189801adc920e827e2446
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2025-10-30 09:28:47 +0000

    use aws tat secret

--- concourse/scripts/redeploy_api.sh
+++ concourse/scripts/redeploy_api.sh
@@ -3,14 +3,21 @@ set -euo pipefail
 
 echo "Redeploying API in ${env}"
 
+aws_account_id=$(echo "$secrets" | jq -r .aws_account_id)
+aws_access_key_id=$(echo "$secrets" | jq -r .aws_access_key_id)
+aws_secret_access_key=$(echo "$secrets" | jq -r .aws_secret_access_key)
+
+export AWS_ACCESS_KEY_ID=$aws_access_key_id
+export AWS_SECRET_ACCESS_KEY=$aws_secret_access_key
+
 # If you already assume role earlier you can source that again; otherwise:
-if [[ -n "${aws_role_arn:-}" ]]; then
-  echo "Assuming role ${aws_role_arn}"
-  # Re‑use existing assume_role.sh if present; else simple sts call:
-  if [[ -f ./resource-repo/concourse/scripts/assume_role.sh ]]; then
-    source ./resource-repo/concourse/scripts/assume_role.sh
-  fi
-fi
+# if [[ -n "${aws_role_arn:-}" ]]; then
+#   echo "Assuming role ${aws_role_arn}"
+#   # Re‑use existing assume_role.sh if present; else simple sts call:
+#   if [[ -f ./resource-repo/concourse/scripts/assume_role.sh ]]; then
+#     source ./resource-repo/concourse/scripts/assume_role.sh
+#   fi
+# fi
 
 api_name="tech-audit-tool"
 

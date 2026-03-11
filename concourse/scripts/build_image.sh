aa053098e52caf66ca357eb09cbe7b9541886cb0
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2026-03-11 12:13:05 +0000

    add git

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -7,6 +7,15 @@ container_image=$(echo "$secrets" | jq -r .container_image)
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
+
+# Configure git to use the GitHub token for HTTPS clones (Poetry git dependencies)
+# This makes "https://github.com/ONS-Innovation/..." authenticate automatically.
+git config --global url."https://${github_access_token}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+
+# (Optional) sanity check without printing the token
+git config --global --get-regexp '^url\..*\.insteadOf$' | sed 's/https:\/\/.*@github.com/https:\/\/***@github.com/'
+
+
 podman build -t ${container_image}:${tag} resource-repo
 
 podman tag ${container_image}:${tag} ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com/${container_image}:${tag}
